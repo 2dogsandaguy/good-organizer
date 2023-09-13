813116412e3c641ac743d2e4c80fdefb2c99a0f7
2dogsandaguy <Neverclear@ymail.com>
2023-09-12 20:36:02 -0400

    worked path to db.json to get info to save

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
   const express = require("express");
   const path = require("path");
-  
   const app = express();
   const PORT = process.env.PORT || 3001;
+  const routes = require("./routes/routes"); // Import the routes
   
-  const routes = require("./routes/routes");
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
   
   app.use(express.static(path.join(__dirname, "./public")));
   
+  // Use the routes from routes.js
   app.use("/", routes);
+  
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+  );
   
   app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
